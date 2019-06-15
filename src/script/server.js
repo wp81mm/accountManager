19b3ba433b8447d3811e8087bc813543d452ccd5
DustinKim <productionkim@gmail.com>
2019-06-16 00:18:36 +0900

    add pug

--- src/script/server.js
+++ src/script/server.js
@@ -3,7 +3,10 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => res.send("home"));
+app.set(`view engine`, "pug");
+app.set(`views`, "./src/views");
+
+app.get("/", (req, res) => res.render("home"));
 
 const handleListening = () =>
   console.log(`✅Listening on: http://localhost:${PORT}`);
