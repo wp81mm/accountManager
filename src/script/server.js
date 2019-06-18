500feca9711a71f8843aef3bcedcb02071228d56
DustinKim <productionkim@gmail.com>
2019-06-18 16:26:09 +0900

    modify server

--- src/script/server.js
+++ src/script/server.js
@@ -1,4 +1,5 @@
-import express from "express";
+// import express from "express";
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -6,7 +7,8 @@ const PORT = process.env.PORT || 4000;
 app.set(`view engine`, "pug");
 app.set(`views`, "./src/views");
 
-app.get("/", (req, res) => res.render("home"));
+// app.get("/", (req, res) => res.render("home"));
+app.get("/", (req, res) => res.send("Hello World"));
 
 const handleListening = () =>
   console.log(`✅Listening on: http://localhost:${PORT}`);
