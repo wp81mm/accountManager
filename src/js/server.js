54481a41a6b6c37171cc9212feb15c48caabf845
DustinKim <productionkim@gmail.com>
2019-07-05 17:29:09 +0900

    test static for heroku

--- src/js/server.js
+++ src/js/server.js
@@ -6,7 +6,7 @@ import globalRouter from "./routers/globalRouter";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use("/js", express.static(`./dist/public/js`)); //프론트 엔드 자바 스크립트 파일을 합쳐서 dist/public/js에 넣어주면 된다.
+app.use("/js", express.static(`dist/public/js`)); //프론트 엔드 자바 스크립트 파일을 합쳐서 dist/public/js에 넣어주면 된다.
 app.use("/css", express.static(`./dist/public/css`)); //css파일을 합쳐서 dist/public/css에 넣어주면 된다.
 
 app.set(`view engine`, "pug");
